b085a990a4e4a01ab4869e7a62bd5ba831bc3c75
Soban S <sobanshafeq123@gmail.com>
2025-03-23 01:38:12 +0500

    learned single child widgets, aka single layout childs

--- lib/main.dart
+++ lib/main.dart
@@ -16,23 +16,43 @@ class MyApp extends StatelessWidget {
               title: const Text("Greetings, Learner!!"),
             ),
             backgroundColor: Colors.blue[100],
-            body: const Center(
-              child: Column(
-                children: [
-                  Text('Hello, World!'),
-                  Text('This is the Adromeda Galaxy:'),
-                  Image(
-                    image: AssetImage('assets/andromeda.jpg'),
+            body: Center(
+              child: Center(
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  color: Colors.orange[900],
+                  child: Center(
+                    child: Text(
+                      'Some text in the container',
+                    ),
                   ),
-                  Text('And this is the Milky Way Galaxy:'),
-                  Image(
-                    image: AssetImage('assets/milkyway.jpg'),
-                  ),
-                  //   image: NetworkImage(
-                  //       'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1msIHD.img'),
-                  // ),
-                ],
+                ),
               ),
+              // child: Column(
+              //   children: [
+              //     Text('Hello, World!'),
+              //     Text('This is the Adromeda Galaxy:'),
+              //     Image(
+              //       image: AssetImage('assets/andromeda.jpg'),
+              //     ),
+              //     Text('And this is the Milky Way Galaxy:'),
+              //     Image(
+              //       image: AssetImage('assets/milkyway.jpg'),
+              //     ),
+              //     //   image: NetworkImage(
+              //     //       'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1msIHD.img'),
+              //     // ),
+              //   ],
+              // ),
             )));
   }
 }
+
+
+
+/*
+NOTES:
+There are two types of widgets...Single child widgets/single layout child (e.g; Center, Container) these accept only one child and Multiple child widgets/Multi layout child (e.g; Column) these accept multiple children.
+
+*/
